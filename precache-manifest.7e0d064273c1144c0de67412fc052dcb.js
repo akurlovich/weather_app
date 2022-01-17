c3a5af52dd9719f89962fa58cea953e1b55571aa
akurlovich <71250705+akurlovich@users.noreply.github.com>
2022-01-17 18:05:28 +0300

    Updates

---
+++ precache-manifest.7e0d064273c1144c0de67412fc052dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4588a3982a255d7be3530f05b1a1c80",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "b38e9f4ad5934dfabfac",
+    "url": "/weather_app/static/css/main.e99ff328.chunk.css"
+  },
+  {
+    "revision": "d2634314d4950dbb85ee",
+    "url": "/weather_app/static/js/2.8df4df93.chunk.js"
+  },
+  {
+    "revision": "b38e9f4ad5934dfabfac",
+    "url": "/weather_app/static/js/main.b0db0d1f.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  },
+  {
+    "revision": "099bddc2dfce226e6353f6e3059b7e1a",
+    "url": "/weather_app/static/media/search-icon.099bddc2.svg"
+  }
+]);
